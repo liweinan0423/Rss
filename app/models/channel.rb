a910194a7afa26d9968f9488fcedd3be6ea36875
liweinan <120113698@qq.com>
2013-04-13 19:52:48 +0800

    add subscribe link

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,6 +1,6 @@
 class Channel < ActiveRecord::Base
   attr_accessible :description, :name
   has_many :posts, :dependent => :destroy
-  has_many :subscriptions, :depencent => :destroy
+  has_many :subscriptions, :dependent => :destroy
   validates :name, :presence => true
 end
