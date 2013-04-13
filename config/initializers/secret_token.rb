0c46aebefa72dc24b381249042652bbb7ee3c01e
liweinan <120113698@qq.com>
2013-04-13 14:05:45 +0800

    generate new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rss::Application.config.secret_token = 'c6b1c0040c95448e4e22b19c811b631adce64248b974ec40b2f8d5443977c0e3ff48ebb764911e7fc9224b342e58f7a933b05bc0108df2bebba3247189ee24be'
